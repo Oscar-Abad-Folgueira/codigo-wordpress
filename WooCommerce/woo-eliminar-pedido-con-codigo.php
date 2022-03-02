f2aed9e0b20bac2de14db97cc855ab9ee4c2c9a0
Oscar Abad Folgueira <oscarabadfolgueira@gmail.com>
2022-03-02 08:56:50 +0100

    WooCommerce Snippet: Eliminar un pedido con código.

---
+++ WooCommerce/woo-eliminar-pedido-con-codigo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @snippet       WooCommerce Snippet: Eliminar un pedido con código.
+ * @author        Oscar Abad Folgueira
+ * @author_url    https://www.oscarabadfolgueira.com
+ * @snippet_url   https://www.oscarabadfolgueira.com/
+ */
+
+wp_delete_post($order_id,true);
