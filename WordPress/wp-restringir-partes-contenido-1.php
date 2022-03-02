d9ed02f01832b7ec789d69182486884ad963d961
Oscar Abad Folgueira <oscarabadfolgueira@gmail.com>
2022-03-02 14:24:19 +0100

    WordPress Snippet: Restringir parte del contenido

---
+++ WordPress/wp-restringir-partes-contenido-1.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @snippet       WordPress Snippet: Restringir parte del contenido
+ * @author        Oscar Abad Folgueira
+ * @author_url    https://www.oscarabadfolgueira.com
+ * @snippet_url   https://www.oscarabadfolgueira.com/crear-shortcode-restringir-partes-del-contenido/
+ */
+
+function oaf_restrict_partial_content_shortcode( $atts, $content = null ) {
+	if ( is_user_logged_in() && !is_null( $content ) && !is_feed() ) {
+		return $content;
+	} else {
+		return __( 'Upps! Este contenido sólo está disponible para usuarios registrados.', 'oaf_rpcs' );
+	}
+}
+
+add_shortcode ('restrict_pcs', 'oaf_restrict_partial_content_shortcode' );
