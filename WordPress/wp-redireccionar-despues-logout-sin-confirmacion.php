b522bde304d4558e901e295c70d113364000174f
Oscar Abad Folgueira <oscarabadfolgueira@gmail.com>
2022-03-22 10:51:48 +0100

    WordPress Snippet: Redireccionar después de salir sin confirmación

---
+++ WordPress/wp-redireccionar-despues-logout-sin-confirmacion.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @snippet       WordPress Snippet: Redireccionar después de salir sin confirmación
+ * @author        Oscar Abad Folgueira
+ * @author_url    https://www.oscarabadfolgueira.com
+ * @snippet_url   https://oscarabadfolgueira.com/
+ */
+
+
+add_action('check_admin_referer', 'logout_without_confirm', 10, 2);
+function logout_without_confirm($action, $result)
+{
+    if ($action == "log-out" && !isset($_GET['_wpnonce'])) {
+        $redirect_to = '/';
+		wp_redirect($redirect_to);
+		exit;
+    }
+}
